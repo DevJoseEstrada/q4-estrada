05842bfce207ee07897a94ea31aed76328f8eb19
DevJoseEstrada <devjmestrada@gmail.com>
2023-10-07 14:18:42 +0200

    correciones for local

--- FrontEnd/src/app/services/project/project.service.ts
+++ FrontEnd/src/app/services/project/project.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Project } from 'src/app/interfaces/project';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.development';
 environment
 @Injectable({
   providedIn: 'root'
